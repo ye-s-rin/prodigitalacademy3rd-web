2e35ad07fcd83e269e8c3590af9d675f18b3c6ee
ye-s-rin <pyr53540@gmail.com>
2024-01-22 11:51:52 +0900

    feat: 06

--- practice/06_average.js
+++ practice/06_average.js
@@ -1,1 +1,14 @@
 /** p.83 연습문제 */
+let strScores = " 90:30:80 ";
+
+let scores = strScores.trim().split(':');
+let math = parseInt(scores[0]);
+let english = parseInt(scores[1]);
+let science = parseInt(scores[2]);
+
+let average = (math+english+science)/3;
+
+console.log(average);
+/**
+66.66666666666667
+ */
