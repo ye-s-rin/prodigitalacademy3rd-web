8fcfeab10b08e7189a7cbd86c068f2015a3587a9
ye-s-rin <pyr53540@gmail.com>
2024-01-22 11:44:39 +0900

    feag: 05번

---
+++ practice/04_templateLiterals.js
@@ -0,0 +1,11 @@
+/** p.75 문자열 리터럴 */
+let name = "myName";
+let multi = `Hello ${name}
+Hello, JS`;
+
+console.log(multi);
+/**
+> node .\04_templateLiterals.js
+Hello myName
+Hello, JS
+ */
---
+++ practice/05_functions.js
@@ -0,0 +1,23 @@
+const intro = "ys tell \"this is js\"";
+console.log(intro.length);
+console.log(intro.charAt(10));
+
+console.log(intro.indexOf("s"));
+console.log(intro.indexOf("1"));
+
+console.log(intro.includes("this is"));
+
+console.log(intro.startsWith("ys"));
+console.log(intro.endsWith("js"));
+
+console.log('-'.repeat(10));
+/**
+20
+h
+1
+-1
+true
+true
+false
+----------
+ */
