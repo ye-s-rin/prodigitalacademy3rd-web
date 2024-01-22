a17556033981541c5b9e4afd76af18c19b29113c
ye-s-rin <pyr53540@gmail.com>
2024-01-23 08:35:39 +0900

    feat: 함수 정의 두 가지 버전

--- practice/09_confirm.js
+++ practice/09_confirm.js
@@ -2,5 +2,13 @@ ask(
     () => confirm("동의하십니까?"),
     function() { alert("동의하셨습니다.") },
     function() { alert("취소 버튼을 누르셨습니다.") }
-   );
-   
+);
+
+function ask(trigFn, resolveFn, rejectFn){
+    trigFn() ? resolveFn() : rejectFn();
+};
+
+const ask = (trigFn, resolveFn, rejectFn) => {
+    trigFn() ? resolveFn() : rejectFn();
+};
+    
