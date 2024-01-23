d3ce0bc6544c580b33ce622d2c18890cb8d6c9e4
ye-s-rin <pyr53540@gmail.com>
2024-01-23 14:50:21 +0900

    feat: button

--- react/my-app/src/components/BlinkComponent.js
+++ react/my-app/src/components/BlinkComponent.js
@@ -3,14 +3,27 @@ import React, { useEffect, useState } from "react";
 export default function BlinkComponent({ text }) {
   const [showText, setShowText] = useState(true);
 
-  useEffect(() => {
-    const timeoutId = setInterval(() => {
-      setShowText((showText) => !showText);
-    }, 1000);
-    return () => {
-      clearInterval(timeoutId);
-    };
-  }, []);
+  //   useEffect(() => {
+  //     const timeoutId = setInterval(() => {
+  //       setShowText((showText) => !showText);
+  //     }, 1000);
+  //     return () => {
+  //       clearInterval(timeoutId);
+  //     };
+  //   }, []);
 
-  return <div>{showText ? text : null}</div>;
+  //   return <div>{showText ? text : null}</div>;
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setShowText(!showText);
+        }}
+      >
+        클릭
+      </button>
+      {showText ? text : null}
+    </div>
+  );
 }
