65ac4a5aeadf619556172c3f8e1b76c12ca3678f
ye-s-rin <pyr53540@gmail.com>
2024-01-23 17:30:05 +0900

    fix: <input>의 onKeyDown과 <button>의 onClick

--- react/my-app/src/components/Todo.js
+++ react/my-app/src/components/Todo.js
@@ -1,33 +1,49 @@
 import React, { useRef, useState } from "react";
 
 export default function Todo() {
+  let i = 0;
   const inputRef = useRef();
   const focusInput = () => {
     inputRef.current.focus();
   };
   const [text, setText] = useState("");
   const [arr, setArr] = useState([]);
+  const activeEnter = (e) => {
+    if (e.key === "Enter") {
+      setArr([text, ...arr]);
+      inputRef.current.value = "";
+    }
+  };
 
   return (
     <div>
-      <button onClick={focusInput}>입력하러 가기</button>
+      {/* <button onClick={focusInput}>입력하러 가기</button> */}
 
       <input
         ref={inputRef}
         type="text"
         onChange={(e) => {
           setText(e.target.value);
-          if (e.key === "Enter") {
-            setArr([text, ...arr]);
-            setText("");
-            inputRef.current.value = "";
-          }
+        }}
+        onKeyDown={(e) => {
+          activeEnter(e);
         }}
       />
 
+      <button
+        onClick={(e) => {
+          console.log(text);
+          setArr([text, ...arr]);
+          inputRef.current.value = "";
+        }}
+      >
+        입력
+      </button>
+
       <div>
         {arr.map((elem) => (
-          <div>{elem}</div>
+          <div key={i++}>{elem}</div>
+          // child in a list should have a unique "key" prop.
         ))}
       </div>
     </div>
