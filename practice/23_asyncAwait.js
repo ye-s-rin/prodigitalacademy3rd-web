ddee4597495d4f971ea161d24efe721a8a5131bf
ye-s-rin <pyr53540@gmail.com>
2024-01-23 09:58:24 +0900

    feat: 23.js

---
+++ practice/23_asyncAwait.js
@@ -0,0 +1,27 @@
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+function fastFunction(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            const result = data * 2;
+            console.log("Fast function done", result); // 20
+            // reject(new Error("임시 에러")); // catch
+            resolve(result); // then
+        }, 1000);
+    });
+}
+
+async function slowFunction(data) {
+    await delay(3000);
+    const result = data + 10;
+    console.log("Slow function done", result); // 20
+    return result;
+}
+
+async function runTasks() {
+    let result = await fastFunction(10);
+    result = await slowFunction(result);
+    console.log("작업완료", result);
+}
+
+runTasks();
