18bf4a94f3d3deb3c17aa9c6b991f54f8f49c396
ye-s-rin <pyr53540@gmail.com>
2024-01-23 15:27:49 +0900

    fix: use useEffect

--- react/my-app/src/components/CountComponent.js
+++ react/my-app/src/components/CountComponent.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function CountComponent() {
   const [count, setCount] = useState(0);
@@ -6,6 +6,17 @@ export default function CountComponent() {
   const addCount = () => {
     setCount(count + 1);
   };
+  useEffect(() => {
+    console.log("데이터 받아오기! (이 함수는 한 번만 실행됩니다.");
+    return () => {
+      console.log(
+        "메모리를 잡아먹으면 리소스 해제 하는 함수를 return 해 주어야 합니다."
+      );
+    };
+  }, []);
+  useEffect(() => {
+    console.log(`카운트가 증가할때마다 실행! \n -count: ${count}`);
+  }, [count]);
   return (
     <div>
       <div>{count}</div>
