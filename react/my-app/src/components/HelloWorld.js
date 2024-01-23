6e5f5a200b5791ecd0f2661751d1cf4319ca59b6
ye-s-rin <pyr53540@gmail.com>
2024-01-23 13:58:44 +0900

    feat: ...\my-app\components

---
+++ react/my-app/src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>THis is My first React Application</p>
+    </div>
+  );
+}
