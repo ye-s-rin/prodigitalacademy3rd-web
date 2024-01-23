d09dfcf3cd9fe48551fb8d73bbfb1cae3e0b7401
ye-s-rin <pyr53540@gmail.com>
2024-01-23 15:32:43 +0900

    style: 개행

--- react/my-app/src/components/CountComponent.js
+++ react/my-app/src/components/CountComponent.js
@@ -6,6 +6,7 @@ export default function CountComponent() {
   const addCount = () => {
     setCount(count + 1);
   };
+
   useEffect(() => {
     console.log("데이터 받아오기! (이 함수는 한 번만 실행됩니다.)");
     return () => {
@@ -14,9 +15,11 @@ export default function CountComponent() {
       );
     };
   }, []);
+
   useEffect(() => {
     console.log(`카운트가 증가할때마다 실행! \n -count: ${count}`);
   }, [count]);
+
   return (
     <div>
       <div>{count}</div>
