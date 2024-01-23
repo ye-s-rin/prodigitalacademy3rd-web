1e6630cef3fa84f80f49afe0e8bf32492b93a3cf
ye-s-rin <pyr53540@gmail.com>
2024-01-23 09:39:47 +0900

    feat: 22_promise.js

---
+++ practice/22_promise.js
@@ -0,0 +1,37 @@
+function fastFunction(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            const result = data * 2;
+            console.log("Fast function done", result); //20
+            // reject(new Error("임시 에러")); // catch
+            resolve(result); // then
+        }, 1000);
+    });
+}
+
+function slowFunction(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(function () {
+            const result = data + 10;
+            console.log("Slow function done", result); //30
+            // reject(new Error("임시 에러")); // catch
+            resolve(result); // then
+        }, 3000);
+    });
+}
+
+function runTasks() {
+    // fastFunction 실행 -> slowFunction 실행
+    return fastFunction(10)
+        .then((data) => {
+            return slowFunction(data);
+        })
+        .then((data) => {
+            console.log("작업완료");
+        })
+        .catch((err) => {
+            console.error(err);
+        });
+}
+
+runTasks();
