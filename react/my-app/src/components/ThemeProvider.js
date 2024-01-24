3a1ad8a41a4f86fdf14eea25a01462d9075f1486
ye-s-rin <pyr53540@gmail.com>
2024-01-24 13:34:07 +0900

    feat: ThemeProvider.js

---
+++ react/my-app/src/components/ThemeProvider.js
@@ -0,0 +1,14 @@
+import React, { useState, useContext, createContext } from "react";
+
+// 테마 컨텍스트 생성
+const ThemeContext = createContext();
+
+// 테마 제공자 컴포넌트
+export function ThemeProvider({ children }) {
+  const [theme, setTheme] = useState("light"); // 초기 테마는 "light"
+  const toggleTheme = () => {
+    setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+  };
+
+  return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+}
