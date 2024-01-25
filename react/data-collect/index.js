b52571e7d58498e0e4e445054954813685a71f78
ye-s-rin <pyr53540@gmail.com>
2024-01-25 16:50:04 +0900

    fix: async & await

--- react/data-collect/index.js
+++ react/data-collect/index.js
@@ -4,27 +4,36 @@ import * as cheerio from "cheerio";
 const url = "https://quotes.toscrape.com/";
 
 async function fetchPageData() {
-  let array = [];
-
   try {
     const response = await axios.get(url);
-    // console.log(response.data);
-    array.push(response.data);
+    return response.data;
   } catch (err) {
     console.error(err);
+    throw err; // 호출자에서 오류를 처리하기 위해 다시 던집니다.
   }
 }
-const html = fetchPageData();
-const $ = cheerio.load(html);
 
-const data = $(".quote .text")
-  .map((idx, el) => {
-    const quote = $(el).text().trim();
-    const author = $(el).find(".author").text().trim();
-    const authorUrl = $(el).find("a").prop("href");
+(async () => {
+  try {
+    const html = await fetchPageData(); // 프로미스가 완료될 때까지 기다립니다.
+    const $ = cheerio.load(html);
 
-    return `${quote} - ${author} - ${authorUrl}`;
-  })
-  .get();
+    const data = $(".quote .text")
+      .map((idx, el) => {
+        const quote = $(el).text().trim();
+        const author = $(el).siblings(".author").text().trim();
+        const authorUrl = $(el).siblings(".author").find("a").prop("href");
 
-console.log(data);
+        return {
+          quote: quote,
+          author: author,
+          authorUrl: authorUrl,
+        };
+      })
+      .get();
+
+    console.log(data);
+  } catch (error) {
+    console.error("error:", error);
+  }
+})();
