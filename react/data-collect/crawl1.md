dfd770f8d201d5125641589e85566b1d49211e32
ye-s-rin <pyr53540@gmail.com>
2024-01-25 16:39:08 +0900

    feat: quotes to scrape

---
+++ react/data-collect/crawl1.md
@@ -0,0 +1,9 @@
+https://quotes.toscrape.com/ 1페이지 크롤링
+
+return data-type: [
+{
+quote: <문구>,
+author: <작성자>,
+authorUrl: <작성자 링크>
+}
+]
--- react/data-collect/index.js
+++ react/data-collect/index.js
@@ -1,15 +1,30 @@
 import axios from "axios";
-const url = "https://www.naver.com";
+import * as cheerio from "cheerio";
+
+const url = "https://quotes.toscrape.com/";
 
 async function fetchPageData() {
   let array = [];
 
   try {
     const response = await axios.get(url);
-    console.log(response.data);
+    // console.log(response.data);
     array.push(response.data);
   } catch (err) {
     console.error(err);
   }
 }
-fetchPageData();
+const html = fetchPageData();
+const $ = cheerio.load(html);
+
+const data = $(".quote .text")
+  .map((idx, el) => {
+    const quote = $(el).text().trim();
+    const author = $(el).find(".author").text().trim();
+    const authorUrl = $(el).find("a").prop("href");
+
+    return `${quote} - ${author} - ${authorUrl}`;
+  })
+  .get();
+
+console.log(data);
