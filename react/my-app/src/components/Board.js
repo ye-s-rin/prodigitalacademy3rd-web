f1b54c6f99f7dd6d90f38a819f3a0e2eb5c0cd0d
ye-s-rin <pyr53540@gmail.com>
2024-01-25 11:01:24 +0900

    sytle: function name

--- react/my-app/src/components/Board.js
+++ react/my-app/src/components/Board.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-export default function Todo() {
+export default function Board() {
   let i = 0;
   const [obj, setObj] = useState({});
   useEffect(() => {
