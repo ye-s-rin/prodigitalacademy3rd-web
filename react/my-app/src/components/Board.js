7cbceca85b57fa4064b011ecf6d052b18be1ac9e
ye-s-rin <pyr53540@gmail.com>
2024-01-25 10:59:15 +0900

    feat: 게시판 조회

---
+++ react/my-app/src/components/Board.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+export default function Todo() {
+  let i = 0;
+  const [obj, setObj] = useState({});
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setObj(json));
+  }, []);
+
+  console.log(obj);
+  return (
+    <div key={i++}>
+      {Object.keys(obj).map((key) => (
+        <div>
+          <h3>{obj[key].title}</h3>
+          <p>{obj[key].body}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  );
+}
