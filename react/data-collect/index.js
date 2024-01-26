fc3d3f55a1e679d9c42905b20cb089c2447ca65b
ye-s-rin <pyr53540@gmail.com>
2024-01-26 11:38:37 +0900

    feat: author detail

--- react/data-collect/index.js
+++ react/data-collect/index.js
@@ -9,33 +9,44 @@ async function fetchPageData() {
     return response.data;
   } catch (err) {
     console.error(err);
-    throw err; // 호출자에서 오류를 처리하기 위해 다시 던집니다.
+    throw err;
   }
 }
 
 (async () => {
   try {
-    const html = await fetchPageData(); // 프로미스가 완료될 때까지 기다립니다.
+    const html = await fetchPageData();
     const $ = cheerio.load(html);
 
-    const data = $(".quote")
-      .map((idx, el) => {
-        const quote = $(el).find(".text").text().trim();
-        const author = $(el).find(".author").text().trim();
-        const authorUrl = $(el).find("a").prop("href");
-        const tags = $(el)
-          .find("a.tag")
-          .map((idx, el) => $(el).text().trim())
-          .get();
+    const data = [];
 
-        return {
-          quote: quote,
-          author: author,
-          authorUrl: authorUrl,
-          tags: tags,
-        };
-      })
-      .get();
+    for (const el of $(".quote")) {
+      const quote = $(el).find(".text").text().trim();
+      const author = $(el).find(".author").text().trim();
+      const authorUrl = url + $(el).find("a").prop("href");
+      const tags = $(el)
+        .find("a.tag")
+        .map((idx, tagEl) => $(tagEl).text().trim())
+        .get();
+
+      let authorDetail = "";
+      try {
+        const response = await axios.get(authorUrl);
+        const htmlAuthor = response.data;
+        const $author = cheerio.load(htmlAuthor);
+        authorDetail = $author(".author-description").text().trim();
+      } catch (err) {
+        console.error(err);
+      }
+
+      data.push({
+        quote: quote,
+        author: author,
+        authorUrl: authorUrl,
+        tags: tags,
+        authorDetail: authorDetail,
+      });
+    }
 
     console.log(data);
   } catch (error) {
