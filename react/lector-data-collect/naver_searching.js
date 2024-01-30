c76e4ba8e3262485226f84db14ddc86314b55eb5
ye-s-rin <pyr53540@gmail.com>
2024-01-30 10:50:27 +0900

    fix

--- react/lector-data-collect/naver_searching.js
+++ react/lector-data-collect/naver_searching.js
@@ -1,38 +1,47 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
 
-const params = {
-  where: "news",
-  query: "이차전지",
-};
-
-async function fetchNaverSearch(params) {
-  const baseUrl = "https://search.naver.com/search.naver";
+async function fetchNaverStock(code) {
+  const baseUrl = "https://finance.naver.com/item/sise_day.naver";
+  const params = {
+    code: code,
+  };
 
   const resp = await axios.get(baseUrl, {
     params: params,
+    headers: {
+      "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    },
   });
   const $ = cheerio.load(resp.data);
-  const result = $(".list_news .bx")
-    .map((i, elem) => {
-      const press = $(elem).find("a.info.press").text();
-      const anchor = $(elem).find("a.news_tit");
-      const title = anchor.text().trim();
-      const url = anchor.prop("href");
-      const dsc = $(elem).find(".news_dsc").text().trim();
-      const imgTag = $(elem).find(".dsc_thumb img");
-      // const imgUrl = imgTag.prop('src');
-      const imgUrl = imgTag.prop("data-lazysrc");
+  const trTags = $("tr");
+  const data = trTags
+    .slice(1, trTags.length - 1)
+    .map((i, el) => {
+      const date = $(el).find("td:nth-child(1)")?.text()?.trim();
+      const close = $(el).find("td:nth-child(2)")?.text()?.trim();
+      const ratio = $(el).find("td:nth-child(3)")?.text()?.trim();
+      const open = $(el).find("td:nth-child(4)")?.text()?.trim();
+      const high = $(el).find("td:nth-child(5)")?.text()?.trim();
+      const low = $(el).find("td:nth-child(6)")?.text()?.trim();
+      const volume = $(el).find("td:nth-child(7)")?.text()?.trim();
 
       return {
-        press: press,
-        title: title,
-        url: url,
-        dsc: dsc,
-        imgUrl: imgUrl,
+        date,
+        close,
+        ratio,
+        open,
+        high,
+        low,
+        volume,
       };
     })
     .get();
+  const result = data.filter((elem, idx) => {
+    return elem.date;
+  });
   console.log(result);
 }
-fetchNaverSearch(params);
+
+fetchNaverStock("005930");
