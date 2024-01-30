4da732452aa3c179d5acf3887107dd36527a109b
ye-s-rin <pyr53540@gmail.com>
2024-01-30 14:50:56 +0900

    feat: (WIP) 크롤링한 이미지 다운로드

---
+++ crawling/data-collect/imgCrawling.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+import * as cheerio from "cheerio";
+
+import fs from "fs";
+
+async function fetchPageData(url) {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+}
+
+async function downloadImage(data) {
+  try {
+    const response = await axios.get(data.image, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(response.data, 'binary');
+    fs.writeFileSync(`./crawledImg/${data.title}.jpg`, buffer);
+    console.log('이미지 다운로드 완료');
+  } catch (error) {
+    console.error('이미지 다운로드 실패:', error.message);
+  }
+}
+
+(async () => {
+  const homeUrl =
+    "https://search.naver.com/search.naver?where=news&sm=tab_jum&query=이차전지";
+  let url = homeUrl;
+  const data = [];
+
+  let pageNum = 0;
+  while (pageNum < 2) {
+    url = homeUrl + "&start=" + String(10 * pageNum + 1);
+    pageNum++;
+    console.log(pageNum);
+    console.log(url);
+
+    try {
+      const html = await fetchPageData(url);
+      const $ = cheerio.load(html);
+
+      for (const el of $(".news_area")) {
+        const title = $(el).find(".news_tit").prop("title");
+        const image = $(el).find(".news_contents a img").prop("data-lazysrc");
+
+        data.push({
+          title: title,
+          image: image,
+          pageNum: pageNum,
+        });
+      }
+      console.log(data);
+    } catch (error) {
+      console.error("error:", error);
+    }
+  }
+
+  for(const el of data){
+    await downloadImage(el);
+  }
+})();
