930d1270047f1ef62fc1a5fcf63866e9a708848f
ye-s-rin <pyr53540@gmail.com>
2024-01-31 14:45:54 +0900

    feat: useState

--- ts/ts-react/my-app/src/App.tsx
+++ ts/ts-react/my-app/src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import CaptionImage from "./compoment/CaptionImage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CaptionImage
+        imgUrl="https://www.bing.com/images/search?view=detailV2&ccid=fq%2bNjU9S&id=21E8F7DF61289D23A51ACD7908BCFC3CB7FA3F4B&thid=OIP.fq-NjU9SpzqC-Mdm5vwUDQHaE8&mediaurl=https%3a%2f%2fget.pxhere.com%2fphoto%2fcloud-sky-atmosphere-daytime-cumulus-clouds-thunderstorm-clouds-form-cumulus-clouds-meteorological-phenomenon-cloud-mountain-cloud-of-bunch-of-atmosphere-of-earth-591971.jpg&exph=3456&expw=5184&q=%ea%b5%ac%eb%a6%84&simid=607996430079832824&FORM=IRPRST&ck=83984CC0FE0CF7D9C67AD30649DDC353&selectedIndex=0&itb=0"
+        text="구름"
+      />
     </div>
   );
 }
---
+++ ts/ts-react/my-app/src/compoment/CaptionImage.tsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+
+export type Props = {
+  imgUrl: string;
+  text: string;
+};
+type ComplexCount = {
+  value: number;
+  mutation: number;
+};
+
+export default function CaptionImage({ imgUrl, text }: Props) {
+  const [count, setCount] = useState<ComplexCount>({ value: 0, mutation: 0 });
+
+  return (
+    <div>
+      <img src="" alt="" />
+      <p></p>
+    </div>
+  );
+}
