16f2689ac1b0400a4d5235b7a2034a9f04bb9a35
ye-s-rin <pyr53540@gmail.com>
2024-01-31 11:49:44 +0900

    feat: decorator - class에 적용

--- ts/src2/example13.js
+++ ts/src2/example13.js
@@ -1,3 +1,4 @@
+"use strict";
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -13,46 +14,18 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var __esDecorate = (this && this.__esDecorate) || function (ctor, descriptorIn, decorators, contextIn, initializers, extraInitializers) {
-    function accept(f) { if (f !== void 0 && typeof f !== "function") throw new TypeError("Function expected"); return f; }
-    var kind = contextIn.kind, key = kind === "getter" ? "get" : kind === "setter" ? "set" : "value";
-    var target = !descriptorIn && ctor ? contextIn["static"] ? ctor : ctor.prototype : null;
-    var descriptor = descriptorIn || (target ? Object.getOwnPropertyDescriptor(target, contextIn.name) : {});
-    var _, done = false;
-    for (var i = decorators.length - 1; i >= 0; i--) {
-        var context = {};
-        for (var p in contextIn) context[p] = p === "access" ? {} : contextIn[p];
-        for (var p in contextIn.access) context.access[p] = contextIn.access[p];
-        context.addInitializer = function (f) { if (done) throw new TypeError("Cannot add initializers after decoration has completed"); extraInitializers.push(accept(f || null)); };
-        var result = (0, decorators[i])(kind === "accessor" ? { get: descriptor.get, set: descriptor.set } : descriptor[key], context);
-        if (kind === "accessor") {
-            if (result === void 0) continue;
-            if (result === null || typeof result !== "object") throw new TypeError("Object expected");
-            if (_ = accept(result.get)) descriptor.get = _;
-            if (_ = accept(result.set)) descriptor.set = _;
-            if (_ = accept(result.init)) initializers.unshift(_);
-        }
-        else if (_ = accept(result)) {
-            if (kind === "field") initializers.unshift(_);
-            else descriptor[key] = _;
-        }
-    }
-    if (target) Object.defineProperty(target, contextIn.name, descriptor);
-    done = true;
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __runInitializers = (this && this.__runInitializers) || function (thisArg, initializers, value) {
-    var useValue = arguments.length > 2;
-    for (var i = 0; i < initializers.length; i++) {
-        value = useValue ? initializers[i].call(thisArg, value) : initializers[i].call(thisArg);
-    }
-    return useValue ? value : void 0;
-};
-var __setFunctionName = (this && this.__setFunctionName) || function (f, name, prefix) {
-    if (typeof name === "symbol") name = name.description ? "[".concat(name.description, "]") : "";
-    return Object.defineProperty(f, "name", { configurable: true, value: prefix ? "".concat(prefix, " ", name) : name });
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 function reportableClassDecorator(constructor) {
     console.log("Decorator 호출");
+    console.log("type of constructor in decorator: " + typeof constructor);
     return /** @class */ (function (_super) {
         __extends(class_1, _super);
         function class_1() {
@@ -63,28 +36,20 @@ function reportableClassDecorator(constructor) {
         return class_1;
     }(constructor));
 }
-var BugReport = function () {
-    var _classDecorators = [reportableClassDecorator];
-    var _classDescriptor;
-    var _classExtraInitializers = [];
-    var _classThis;
-    var BugReport = _classThis = /** @class */ (function () {
-        function BugReport_1(t) {
-            this.type = "report";
-            this.title = t;
-        }
-        return BugReport_1;
-    }());
-    __setFunctionName(_classThis, "BugReport");
-    (function () {
-        var _metadata = typeof Symbol === "function" && Symbol.metadata ? Object.create(null) : void 0;
-        __esDecorate(null, _classDescriptor = { value: _classThis }, _classDecorators, { kind: "class", name: _classThis.name, metadata: _metadata }, null, _classExtraInitializers);
-        BugReport = _classThis = _classDescriptor.value;
-        if (_metadata) Object.defineProperty(_classThis, Symbol.metadata, { enumerable: true, configurable: true, writable: true, value: _metadata });
-        __runInitializers(_classThis, _classExtraInitializers);
-    })();
-    return BugReport = _classThis;
-}();
+// new (...args: any[]) -> 생성자 함수를 의미
+var BugReport = /** @class */ (function () {
+    function BugReport(t) {
+        this.type = "report";
+        console.log(t + " in class");
+        this.title = t;
+    }
+    BugReport = __decorate([
+        reportableClassDecorator,
+        __metadata("design:paramtypes", [String])
+    ], BugReport);
+    return BugReport;
+}());
 var bug = new BugReport("Needs dark mode");
 console.log(bug.title); // Prints "Needs dark mode"
 console.log(bug.type); // Prints "report
+console.log("type of bug instance: " + typeof bug);
--- ts/src2/example13.ts
+++ ts/src2/example13.ts
@@ -2,16 +2,19 @@ function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
   constructor: T
 ) {
   console.log("Decorator 호출");
+  console.log("type of constructor in decorator: " + typeof constructor);
   return class extends constructor {
     reportingURL = "http://www...";
   };
 }
+// new (...args: any[]) -> 생성자 함수를 의미
 
 @reportableClassDecorator
 class BugReport {
   type = "report";
   title: string;
   constructor(t: string) {
+    console.log(t + " in class");
     this.title = t;
   }
 }
@@ -19,3 +22,4 @@ class BugReport {
 const bug = new BugReport("Needs dark mode");
 console.log(bug.title); // Prints "Needs dark mode"
 console.log(bug.type); // Prints "report
+console.log("type of bug instance: " + typeof bug);
