0eb14b33f020ce9c8483e6b71acb1454cd95f257
ye-s-rin <pyr53540@gmail.com>
2024-01-31 10:39:42 +0900

    interface만 상속과 구현이 가능

--- ts/src/example12.ts
+++ ts/src/example12.ts
@@ -17,3 +17,9 @@ type IAdvancedTypeTodo = TypeTodo & {
   completed: boolean;
   etc: string;
 };
+
+/**
+interface만 extends(상속)와 implements(구현) 가능
+typeScript에서 interface의 역할
+1. 타입 지정 2. 추상클래스
+ */
