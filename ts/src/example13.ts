7ec72c98b03726da61249fb3dcaf4e981720694d
ye-s-rin <pyr53540@gmail.com>
2024-01-31 10:48:55 +0900

    feat: Decorator

---
+++ ts/src/example13.ts
@@ -0,0 +1,19 @@
+function reportableClassDecorator<T extends { new (...args: any[]): {} }>(
+  constructor: T
+) {
+  console.log("Decorator 호출");
+  return class extends constructor {
+    reportingURL = "http://www...";
+  };
+}
+@reportableClassDecorator
+class BugReport {
+  type = "report";
+  title: string;
+  constructor(t: string) {
+    this.title = t;
+  }
+}
+const bug = new BugReport("Needs dark mode");
+console.log(bug.title); // Prints "Needs dark mode"
+console.log(bug.type); // Prints "report
