a831d3cc5b347449d20ca181b9f58a04c9b77232
ye-s-rin <pyr53540@gmail.com>
2024-02-01 16:00:47 +0900

    feat: mongo 실습

---
+++ mongodb/mongoPerson.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+const MONGO_HOST = "mongodb+srv://admin:admin@cluster0.uei2mzt.mongodb.net/";
+mongoose
+  .connect(MONGO_HOST, {
+    retryWrites: true,
+    w: "majority",
+  })
+  .then((resp) => {
+    console.log(resp);
+    console.log("DB 연결 성공");
+  });
+const personSchema = new mongoose.Schema({
+  name: {
+    first: String,
+    last: String,
+  },
+  age: Number,
+});
+const Person = mongoose.model("Person", personSchema);
+
+Person.insertMany([
+  {
+    name: {
+      first: "윤수",
+      last: "신",
+    },
+    age: 16,
+  },
+  {
+    name: {
+      first: "민수",
+      last: "남궁",
+    },
+    age: 20,
+  },
+]).then((data) => {
+  console.log(data);
+});
+
+Person.find({
+  age: { $gt: 17, $lt: 66 },
+})
+  .limit(10)
+  .sort({ age: -1 })
+  .select({ name: 1, age: 1 })
+  .then((data) => {
+    console.log(data);
+  });
