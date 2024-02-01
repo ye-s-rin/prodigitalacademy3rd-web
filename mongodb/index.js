8a5e76ee7ceadf1e8052a37dfb4235c5d8151cc2
ye-s-rin <pyr53540@gmail.com>
2024-02-01 13:25:00 +0900

    feat: Mongoose 연결하기

---
+++ mongodb/index.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const MONGO_HOST = "mongodb+srv://admin:admin@cluster0.uei2mzt.mongodb.net/";
+mongoose
+  .connect(MONGO_HOST, {
+    retryWrites: true,
+    w: "majority",
+  })
+  .then((resp) => {
+    console.log(resp);
+    console.log("DB 연결 성공");
+  });
