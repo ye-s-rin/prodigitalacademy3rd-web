63b9e0913a2b28e72164dbf395d6ccc20fcdab35
ye-s-rin <pyr53540@gmail.com>
2024-02-01 13:42:11 +0900

    feat: insertMany()

--- mongodb/index.js
+++ mongodb/index.js
@@ -24,3 +24,15 @@ Cat.create({ name: "야옹이" }).then((data) => {
   console.log("저장된 데이터");
   console.log(data);
 });
+
+Cat.insertMany([
+  {
+    name: "나비",
+  },
+  {
+    name: "부엉이",
+  },
+]).then((data) => {
+  console.log("저장된 데이터");
+  console.log(data);
+});
