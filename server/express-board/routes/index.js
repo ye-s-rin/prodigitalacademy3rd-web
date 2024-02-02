38100b20b223b083763a5678af917caa59986071
ye-s-rin <pyr53540@gmail.com>
2024-02-02 10:44:41 +0900

    refector: 기존 view 삭제

--- server/express-board/routes/index.js
+++ server/express-board/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '박예린' });
+  res.render('index', { title: 'test' });
 });
 
 module.exports = router;
