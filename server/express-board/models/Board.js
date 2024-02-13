6bcb7f5576df3eadf336ca6b14b6130f5fc0b49c
ye-s-rin <pyr53540@gmail.com>
2024-02-13 09:26:52 +0900

    fix: author, updatedAt 추가 + comments 삭제

--- server/express-board/models/Board.js
+++ server/express-board/models/Board.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const boardSchema = new mongoose.Schema({
     title: {type:String, required: true},
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     content: {type:String, required: true},
     author: String,
     createdAt: {type: Date, default: Date.now},
-    comments: {type: Array, default: []},
+    // comments: {type: Array, default: []},
+    updatedAt: {type: Date, default: Date.now},
 })
 const Board = mongoose.model("Board", boardSchema);
 module.exports = Board
