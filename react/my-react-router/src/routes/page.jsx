b7b1690fcddf0966c3e1ffd17b65ef06223b1790
ye-s-rin <pyr53540@gmail.com>
2024-02-14 09:22:35 +0900

    react router

---
+++ react/my-react-router/src/routes/page.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+export default function MainPage() {
+    return (
+        <div>
+            <h1>This is my MainPage</h1>
+            <p>MainPage입니다.</p>
+        </div>
+    );
+}
