38e3c70cb23e9ee05af525e599ec1db6ae40e8b4
ye-s-rin <pyr53540@gmail.com>
2024-02-22 18:07:50 +0900

    글씨 색 및 크기

--- homework/my-homework/src/components/CampaignList.js
+++ homework/my-homework/src/components/CampaignList.js
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react';
 import { fetchCampaignList } from '../apis/campaign';
 
 const CampaignCard = ({ campaign }) => {
+    console.log(campaign);
     return (
         <div className="card" style={{ width: "18rem" }}>
             <img src={campaign.photoUrl} className="card-img-top" alt="..." />
             <div className="card-body">
+                <h3 className="card-title" style={{ color: "#00C2BF" }}>{campaign.achievementRate}</h3>
                 <h5 className="card-title">{campaign.title}</h5>
+                <p className="card-text" style={{ fontSize: "0.8rem", color: "gray" }}>{campaign.nickname}</p>
                 <p className="card-text">{campaign.coreMessage}</p>
             </div>
         </div>
